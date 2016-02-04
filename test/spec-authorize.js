d4e147a9dbc2194c84ec037e97389b980f0994a5
Mohammad Rahhal <m.r992@hotmail.com>
2016-02-04 11:40:24 +0200

    * beautify

--- test/spec-authorize.js
+++ test/spec-authorize.js
@@ -1,25 +1,44 @@
-describe('authorize', function() {
+describe('authorize', function () {
 	var authed = false,
 		policies = [],
 		authorize;
 
 	beforeEach(module('mr.authorize', function ($provide, authorizeProvider, $stateProvider) {
-		authorizeProvider.authed = function () { return authed; };
-		authorizeProvider.policies = function () { return policies; };
+		authorizeProvider.authed = function () {
+			return authed;
+		};
+		authorizeProvider.policies = function () {
+			return policies;
+		};
 
 		$stateProvider
 			.state('home', {})
 			.state('home.sub', {})
-			.state('home.protected', { auth: true })
-
-			.state('protected1', { auth: true })
+			.state('home.protected', {
+				auth: true
+			})
+			.state('protected1', {
+				auth: true
+			})
 			.state('protected1.sub', {})
-			.state('protected1.override', { auth: { override: true } })
-			.state('protected2', { auth: 'p1' })
+			.state('protected1.override', {
+				auth: {
+					override: true
+				}
+			})
+			.state('protected2', {
+				auth: 'p1'
+			})
 			.state('protected2.sub', {})
-			.state('protected3', { auth: ['p1'] })
+			.state('protected3', {
+				auth: ['p1']
+			})
 			.state('protected3.sub', {})
-			.state('protected4', { auth: { policies: ['p1', 'p2'] }});
+			.state('protected4', {
+				auth: {
+					policies: ['p1', 'p2']
+				}
+			});
 	}));
 
 	beforeEach(inject(function (_authorize_) {
@@ -31,7 +50,7 @@ describe('authorize', function() {
 		policies = [];
 	});
 
-	it('should pass with no protection', function() {
+	it('should pass with no protection', function () {
 		expect(authorize('home')).toBe(true);
 	});
 
